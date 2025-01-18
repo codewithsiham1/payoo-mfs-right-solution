f1c2e1ca621a54d017b28f9e8b16cbff8d29a16a
moudud <sihamislam18@gmail.com>
2025-01-18 22:21:00 +0600

    first commits the dom manupulation

--- home.js
+++ home.js
@@ -1,1 +1,40 @@
-console.log('home addedgit')
+// s-1:add event handler prevent page reload after the submit
+// s-2:get money to be  added to the  account balance
+// s-3:get the Phone Number
+// s-4:get the pin number
+// s-5:verify the pin number
+// s-6:get the current balance
+// s-7:ad addMoneyInput with balnce
+// s-8:update the balance in the ui/dom
+// add money to the account
+// step-1: add an event eventhandler to the add money buton inside the form
+document.getElementById('button-add-money')
+.addEventListener('click',function(event){
+    // prevent page reload after the submit
+    event.preventDefault();
+    // step-2:get money to be  added to the  account balance
+    const addMoneyInput=document.getElementById('input-add-money').value;
+    // sterp-3:get the phone number
+    const addPhoneNumber=document.getElementById('phones-number').value;
+//    step-4:get the phone number
+const addPinNumber=document.getElementById('pins-number').value;
+    console.log(addMoneyInput,addPhoneNumber,addPinNumber);
+    // step-5: verify the pin number
+    // wrong way tho validate pin number
+    if(addPinNumber==='1234'){
+        console.log('adding money suceesfully')
+        // step-6:get the current balance
+             const balance=document.getElementById('balance').innerText;
+             console.log(balance)
+            //  step-7: ad addMoneyInput with balance
+            const addMoneyNumber=parseFloat(addMoneyInput);
+            const balanceNumber=parseFloat(balance);
+            const newBalance=addMoneyNumber + balanceNumber;
+            console.log(newBalance)
+            // step-6:update the balance in the ui/dom
+            document.getElementById('balance').innerText=newBalance;
+    }else{
+        alert('failed to added money')
+    }
+
+});
